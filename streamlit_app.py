9063eef401704dc9bb9e5c74d60a6fc3bd92f07d
ekramhajji2024 <ekramhajji7@gmail.com>
2024-07-20 00:15:52 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,23 @@ import os
 # Charger les données
 data = pd.read_csv('data/gdp_data.csv')
 csv_path = 'Heart_Disease_Prediction.csv'
-
+#corr
+try:
+    if os.path.exists(scaler_path):
+        scaler = joblib.load(scaler_path)
+        st.write("Scaler loaded successfully.")
+    else:
+        st.error(f"Scaler file '{scaler_path}' not found.")
+except Exception as e:
+    st.error(f"An error occurred while loading the scaler: {e}")
+try:
+    if os.path.exists(model_path):
+        model = joblib.load(model_path)
+        st.write("Model loaded successfully.")
+    else:
+        st.error(f"Model file '{model_path}' not found.")
+except Exception as e:
+    st.error(f"An error occurred while loading the model: {e}")
 # Titre de l'application
 st.title("Prédiction du Risque Cardiovasculaire")
 
