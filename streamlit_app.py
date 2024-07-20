f9d775d384403c2ebed25d0f091deee1d86bd474
ekramhajji2024 <ekramhajji7@gmail.com>
2024-07-20 07:05:08 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ csv_path = 'Heart_Disease_Prediction.csv'
 # Chemins vers les fichiers du modèle et du scaler
 model_path = 'heart_disease_model.pkl'
 scaler_path = 'scaler.pkl'
-scaler = joblib.load(scale_path)
+scaler = joblib.load(scaler_path)
 model = joblib.load(model_path)
        
 # corr
